b66f2380f959d89972c28d69835efb568dc186a5
john2k <ortiz.jonathan@live.com>
2022-06-25 15:13:40 -0300

    carrusel, inicio y final

--- src/js/carruse.js
+++ src/js/carruse.js
@@ -3,11 +3,13 @@ const atras = document.querySelector(".atras");
 const adelante = document.querySelector(".adelante");
 
 adelante.addEventListener("click", function () {
-  carrusel.scrollLeft != carrusel.scrollWidth
-    ? (carrusel.scrollLeft += carrusel.scrollWidth / 4)
-    : (carrusel.scrollLeft = carrusel.scrollWidth);
+  carrusel.scrollLeft += carrusel.offsetWidth;
 
+  if (carrusel.scrollLeft >= 3500) {
+    carrusel.scrollLeft = 0;
+  }
   console.log(carrusel.scrollLeft);
+  console.log(carrusel.scrollWidth);
 });
 
 atras.addEventListener("click", function () {
@@ -15,4 +17,7 @@ atras.addEventListener("click", function () {
     ? (carrusel.scrollLeft -= carrusel.scrollWidth / 4)
     : (carrusel.scrollLeft = carrusel.scrollWidth);
 
+  if (carrusel.scrollLeft <= 0) {
+    carrusel.scrollLeft = carrusel.scrollWidth;
+  }
 });
