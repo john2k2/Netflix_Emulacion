e1a71c4ced9651426a1c3230ba9d89d17ba57938
john2k2 <ortiz.jonathan@live.com>
2022-06-26 20:15:21 -0300

    asdsadasd

---
+++ carruse.js
@@ -0,0 +1,23 @@
+const carrusel = document.querySelector(".pelicula");
+const atras = document.querySelector(".atras");
+const adelante = document.querySelector(".adelante");
+
+adelante.addEventListener("click", function () {
+  carrusel.scrollLeft += carrusel.offsetWidth;
+
+  if (carrusel.scrollLeft >= carrusel.offsetWidth * 4) {
+    carrusel.scrollLeft = 0;
+  }
+  console.log(carrusel.scrollLeft);
+  console.log(carrusel.scrollWidth);
+});
+
+atras.addEventListener("click", function () {
+  carrusel.scrollLeft != carrusel.scrollWidth
+    ? (carrusel.scrollLeft -= carrusel.scrollWidth / 4)
+    : (carrusel.scrollLeft = carrusel.scrollWidth);
+
+  if (carrusel.scrollLeft <= 0) {
+    carrusel.scrollLeft = carrusel.scrollWidth;
+  }
+});
