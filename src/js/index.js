8a3233d19467efcb8225d0b3a5c1a6eadd09b8be
john2k <ortiz.jonathan@live.com>
2022-07-13 17:52:55 -0300

    cambios para desktop

--- src/js/index.js
+++ src/js/index.js
@@ -22,13 +22,6 @@ atras.addEventListener("click", function () {
   }
 });
 
-function nombre() {
-  let nombre = localStorage.getItem("usuario");
-  let usuario = document.getElementById("usuario");
-  usuario.innerText = nombre;
-}
-nombre();
-
 const ultimas = document.getElementById("pelicula");
 fetch(
   "https://api.themoviedb.org/3/movie/popular?api_key=3e5f0bebd9cfb9a96c31b4a62cb78a30"
@@ -41,3 +34,21 @@ fetch(
       ultimas.appendChild(img);
     });
   });
+
+const btn = document.getElementById("boton2");
+const mostrar = document.getElementById("search1");
+btn.addEventListener("click", function () {
+  mostrar.classList.toggle("ocultar");
+  mostrar.classList.toggle("search2");
+});
+
+const negra = document.getElementById("barra");
+
+negra.addEventListener("scroll", function () {
+  if (negra.scrollTop > 0) {
+    negra.classList.add("negra");
+  } else {
+    negra.classList.remove("negra");
+  }
+}
+);
